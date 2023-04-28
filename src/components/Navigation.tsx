7b43091d947c01ebaca50ce5f00a5df9444d57b9
sangay wangchuk <sangaywangchuk100@gmail.com>
2023-04-28 16:43:43 +0600

    Image optimization

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -113,14 +113,6 @@ export default function HeaderNav() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
